c3991fcf96177d62ddc3a1b8fbc47d62cd19e061
arjun <vsarjun51@gmail.com>
2026-07-19 11:26:32 +0530

    Load SECRET_KEY from .env instead of hardcoding

--- p1/p1/settings.py
+++ p1/p1/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-imr_n9z4hfm)*kgr^yzk70$dh-bevgawb(tp(_0k2y+5+r&t2f'
+SECRET_KEY = SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG") == "True"
